3dcb826c823f98fb18bd752a741709a2c6177c23
PitixJakob <mollyjakob@gmail.com>
2016-09-15 00:01:18 +0200

    JS

---
+++ main.js
@@ -0,0 +1,18 @@
+/**
+ * Created by jakob on 14-09-16.
+ */
+
+
+function go(name) {
+    alert(name);
+}
+
+go("Jakob");
+
+var test = [1, 2 ,3 ,4];
+
+go(test)
+
+for (var i=0; i < 100; i++){
+
+}
